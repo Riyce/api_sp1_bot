fbfea4d855ce1d3cb54ee035bd96bfce5315d1d4
Riyce <your_email@whatever.com>
2020-12-30 00:28:50 +0300

    second try

--- homework.py
+++ homework.py
@@ -6,57 +6,60 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-
 load_dotenv()
-logging.basicConfig(
-    level=logging.INFO,
-    filename='main.log',
-    filemode='w',
-    format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
-)
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-STATUSES = ['reviewing', 'approved', 'rejected']
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+ANSWER = 'У вас проверили работу "{}"!\n\n'
+STATUSES = {
+    'reviewing': 'Ваша работа принята к рассмотрению.',
+    'approved':
+    ANSWER + 'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+    'rejected': ANSWER + 'К сожалению в работе нашлись ошибки.',
+}
 
 
 def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
+    name = homework.get('homework_name')
     status = homework.get('status')
-    if status not in STATUSES or homework_name is None:
+    if status not in STATUSES:
         logging.error('Ошибка ответа от ЯП.')
-    if status == 'reviewing':
-        return 'Работа взята на рассмотрение.'
-    if status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'Неизвестный статус работы. {name}'
+    verdict = STATUSES[status]
+    return verdict.format(name)
 
 
 def get_homework_statuses(current_timestamp):
-    try:
-        time = int(current_timestamp)
-    except ValueError:
-        logging.error('Ожидаемый тип данных — число!')
     params = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(URL, headers=headers, params=params)
-    except requests.RequestException as e:
-        logging.error('Невозможно получить ответ от ЯП.')
-        return e
-    return homework_statuses.json()
+        response = requests.get(URL, headers=HEADERS, params=params)
+    except requests.RequestException:
+        logging.error(
+            f'Невозможно получить ответ от ЯП.\n{URL}\n'
+            f'Параметры запроса: {HEADERS} \n {params}'
+        )
+        return {}
+    answer = response.json()
+    if 'code' in answer.keys():
+        error = answer.get('message')
+        logging.error(f'Ошибка подключения к ЯП.\n {error}')
+        return {}
+    if answer.get('homeworks') is None:
+        logging.error('Вернулся неожиданный ответ.')
+        return {}
+    return answer
 
 
-def send_message(message, bot_client):
+def send_message(message, bot_client=None):
+    logging.info(f'Отправлено сообщение:\n"{message}"')
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    bot_client = telegram.Bot(token=str(TELEGRAM_TOKEN))
+    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     logging.debug('Бот запущен.')
     while True:
@@ -67,16 +70,21 @@ def main():
                     parse_homework_status(new_homework.get('homeworks')[0]),
                     bot_client
                 )
-                logging.info('Сообщение отправлено.')
             current_timestamp = (
                 new_homework.get('current_date', current_timestamp)
             )
             time.sleep(300)
 
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+        except Exception as error:
+            logging.error(f'Бот столкнулся с ошибкой: {error}')
             time.sleep(5)
 
+
+logging.basicConfig(
+    handlers=[logging.FileHandler(__file__ + '.log', 'w', 'utf-8')],
+    level=logging.INFO,
+    format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
+)
 
 if __name__ == '__main__':
     main()
