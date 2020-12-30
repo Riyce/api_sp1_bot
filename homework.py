2771b7cab0984e61719a1634e4727c64203d999a
Riyce <your_email@whatever.com>
2020-12-31 02:04:02 +0300

    after review

--- homework.py
+++ homework.py
@@ -13,23 +13,40 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-ANSWER = 'У вас проверили работу "{}"!\n\n'
 STATUSES = {
     'reviewing': 'Ваша работа принята к рассмотрению.',
     'approved':
-    ANSWER + 'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
-    'rejected': ANSWER + 'К сожалению в работе нашлись ошибки.',
+    'У вас проверили работу "{name}"!\n\n' +
+    'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+    'rejected':
+    'У вас проверили работу "{name}"!\n\n ' +
+    'К сожалению в работе нашлись ошибки.',
 }
+STATUS_ERROR_MESSAGE = 'Неизвестный статус работы.\n{status}'
+REQUEST_ERROR_MESSAGE = (
+    'Невозможно получить ответ от ЯП.\n {url}\n ' +
+    'Заголовок запроса: {headers}\n ' +
+    'Параметры запроса: {params}'
+)
+JSON_ERROR_MESSAGE = (
+    'Возникла ошибка при получении ответа от ЯП.\n {url}\n ' +
+    'Заголовок запроса: {headers}\n ' +
+    'Параметры запроса: {params}\n ' +
+    'Ошибка: {code}'
+)
+CONNECTION_ERROR_MESSAGE = 'Ошибка подключения'
+BOT_ERROR_MESSAGE = 'Бот столкнулся с ошибкой:\n{error}'
+BOT_START_MESSAGE = 'Бот запущен.'
+MESSAGE = 'Отправлено сообщение:\n {message}'
 
 
 def parse_homework_status(homework):
     name = homework.get('homework_name')
     status = homework.get('status')
     if status not in STATUSES:
-        logging.error('Ошибка ответа от ЯП.')
-        return f'Неизвестный статус работы. {name}'
+        raise KeyError(STATUS_ERROR_MESSAGE.format(status=status))
     verdict = STATUSES[status]
-    return verdict.format(name)
+    return verdict.format(name=name)
 
 
 def get_homework_statuses(current_timestamp):
@@ -38,37 +55,43 @@ def get_homework_statuses(current_timestamp):
         response = requests.get(URL, headers=HEADERS, params=params)
     except requests.RequestException:
         logging.error(
-            f'Невозможно получить ответ от ЯП.\n{URL}\n'
-            f'Параметры запроса: {HEADERS} \n {params}'
+            REQUEST_ERROR_MESSAGE.format(
+                url=URL,
+                headers=HEADERS,
+                params=params
+            )
         )
-        return {}
+        raise ConnectionError(CONNECTION_ERROR_MESSAGE)
     answer = response.json()
-    if 'code' in answer.keys():
-        error = answer.get('message')
-        logging.error(f'Ошибка подключения к ЯП.\n {error}')
-        return {}
-    if answer.get('homeworks') is None:
-        logging.error('Вернулся неожиданный ответ.')
-        return {}
+    if 'code' in answer or 'error' in answer:
+        code = answer.get('code')
+        raise Exception(
+            JSON_ERROR_MESSAGE.format(
+                url=URL,
+                headers=HEADERS,
+                params=params,
+                code=code,
+            )
+        )
     return answer
 
 
-def send_message(message, bot_client=None):
-    logging.info(f'Отправлено сообщение:\n"{message}"')
+def send_message(message, bot_client):
+    logging.info(MESSAGE.format(message=message))
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    logging.debug('Бот запущен.')
+    logging.debug(BOT_START_MESSAGE)
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(new_homework.get('homeworks')[0]),
-                    bot_client
+                    bot_client,
                 )
             current_timestamp = (
                 new_homework.get('current_date', current_timestamp)
@@ -76,15 +99,14 @@ def main():
             time.sleep(300)
 
         except Exception as error:
-            logging.error(f'Бот столкнулся с ошибкой: {error}')
+            logging.error(BOT_ERROR_MESSAGE.format(error=error))
             time.sleep(5)
 
 
-logging.basicConfig(
-    handlers=[logging.FileHandler(__file__ + '.log', 'w', 'utf-8')],
-    level=logging.INFO,
-    format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
-)
-
 if __name__ == '__main__':
+    logging.basicConfig(
+        handlers=[logging.FileHandler(__file__ + '.log', 'w', 'utf-8')],
+        level=logging.INFO,
+        format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
+    )
     main()
