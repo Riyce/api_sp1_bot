786a23f6cc96e46d0512b66797ae612b54722501
Riyce <your_email@whatever.com>
2021-01-02 16:48:58 +0300

    !!!

--- homework.py
+++ homework.py
@@ -14,63 +14,74 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 STATUSES = {
-    'reviewing': 'Ваша работа принята к рассмотрению.',
+    'reviewing':
+        'Ваша работа принята к рассмотрению.',
     'approved':
-    'У вас проверили работу "{name}"!\n\n' +
-    'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+        'У вас проверили работу "{name}"!\n\n' +
+        'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
     'rejected':
-    'У вас проверили работу "{name}"!\n\n ' +
-    'К сожалению в работе нашлись ошибки.',
+        'У вас проверили работу "{name}"!\n\n ' +
+        'К сожалению в работе нашлись ошибки.',
 }
 STATUS_ERROR_MESSAGE = 'Неизвестный статус работы.\n{status}'
 REQUEST_ERROR_MESSAGE = (
-    'Невозможно получить ответ от ЯП.\n {url}\n ' +
-    'Заголовок запроса: {headers}\n ' +
-    'Параметры запроса: {params}'
+    'Невозможно получить ответ от ЯП.\n'
+    '{url}\n '
+    'Заголовок запроса: {headers}\n '
+    'Параметры запроса: {params}\n'
+    '{exception}'
 )
 JSON_ERROR_MESSAGE = (
-    'Возникла ошибка при получении ответа от ЯП.\n {url}\n ' +
-    'Заголовок запроса: {headers}\n ' +
-    'Параметры запроса: {params}\n ' +
-    'Ошибка: {code}'
+    'Возникла ошибка при получении ответа от ЯП.\n'
+    '{url}\n '
+    'Заголовок запроса: {headers}\n '
+    'Параметры запроса: {params}\n '
+    'Ошибка: {error}\n'
 )
-CONNECTION_ERROR_MESSAGE = 'Ошибка подключения'
-BOT_ERROR_MESSAGE = 'Бот столкнулся с ошибкой:\n{error}'
+BOT_ERROR_MESSAGE = 'Бот столкнулся с ошибкой:\n {error}'
 BOT_START_MESSAGE = 'Бот запущен.'
 MESSAGE = 'Отправлено сообщение:\n {message}'
 
 
 def parse_homework_status(homework):
-    name = homework.get('homework_name')
-    status = homework.get('status')
+    status = homework['status']
     if status not in STATUSES:
-        raise KeyError(STATUS_ERROR_MESSAGE.format(status=status))
-    verdict = STATUSES[status]
-    return verdict.format(name=name)
+        raise ValueError(STATUS_ERROR_MESSAGE.format(status=status))
+    return STATUSES[status].format(name=homework['homework_name'])
 
 
 def get_homework_statuses(current_timestamp):
     params = {'from_date': current_timestamp}
     try:
         response = requests.get(URL, headers=HEADERS, params=params)
-    except requests.RequestException:
-        logging.error(
+    except requests.RequestException as exception:
+        raise ConnectionError(
             REQUEST_ERROR_MESSAGE.format(
                 url=URL,
                 headers=HEADERS,
-                params=params
+                params=params,
+                exception=exception,
             )
         )
-        raise ConnectionError(CONNECTION_ERROR_MESSAGE)
     answer = response.json()
-    if 'code' in answer or 'error' in answer:
-        code = answer.get('code')
-        raise Exception(
+    if 'code' in answer:
+        error = answer['code']
+        raise ValueError(
             JSON_ERROR_MESSAGE.format(
                 url=URL,
                 headers=HEADERS,
                 params=params,
-                code=code,
+                error=error,
+            )
+        )
+    if 'error' in answer:
+        error = answer['error']
+        raise ValueError(
+            JSON_ERROR_MESSAGE.format(
+                url=URL,
+                headers=HEADERS,
+                params=params,
+                error=error,
             )
         )
     return answer
